51f672157f30a47efef1f2b813e45b6020d3bb49
Eduardo Fernandes de Pinho Tavares <161468833+Darknet2099@users.noreply.github.com>
2024-04-04 19:46:20 -0300

    Create Vector exercise 12 .java

---
+++ Vector exercise 12 .java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Os números pares entre 1 e 50 são:");
+
+        for (int x = 2; x <= 50; x += 2) {
+            System.out.println(x);
+        }
+    }
+}
