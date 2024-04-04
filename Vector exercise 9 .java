e9502c9aa6b8b5b01e5a47c9bf6add47695cadbf
Eduardo Fernandes de Pinho Tavares <161468833+Darknet2099@users.noreply.github.com>
2024-04-04 16:06:53 -0300

    Create Vector exercise 9 .java

---
+++ Vector exercise 9 .java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int quantidade;
+    System.out.println("Digite a quantidade de recibos que deseja imprimir: ");
+    quantidade = input.nextInt();
+    int x = 1;
+    while(i <= quantidade){
+      System.out.println("Recibos " + i);
+      i++;
+    }
+  }
+}
